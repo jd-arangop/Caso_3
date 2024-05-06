f13316f3e898c197f2202bc2edc34eccfca781f4
Juliana Ahumada <j.ahumadaa@uniandes.edu.co>
2024-05-05 19:43:11 -0500

    Cantidad de Delegados

--- src/App.java
+++ src/App.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Por favor, ingresa la cantidad de delegados deseados: ");
+        int delegados = scanner.nextInt();
+
         Server server = new Server();
         server.start();
 
         Client.publicKey = Server.publicKey;
-        Client client = new Client(0);
-        client.start();
+
+        for (int i = 0; i < delegados; i++){
+            Client client = new Client(0);
+            client.start();
+        }
+        scanner.close();
     }
 }
